d69728b6de6b8ca309b6924c7457dadb8bc5488a
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2020-02-07 08:25:13 -0800

    Update and rename Broken Calculator to Broken Calculator.java

--- Broken Calculator.java
+++ Broken Calculator.java
@@ -4,7 +4,7 @@ SC: O(1)
 Runtime: 59 ms, faster than 6.66% of Java online submissions for Broken Calculator.
 Memory Usage: 38.3 MB, less than 14.29% of Java online submissions for Broken Calculator.
 
-Approach: Trying to reach X from Y instead of Y from X. 
+Approach: Trying to reach X from Y instead of Y from X.We can also try starting with X, but it will be 2^n combinations which give TLE
 
 class Solution {
     public int brokenCalc(int X, int Y) {
